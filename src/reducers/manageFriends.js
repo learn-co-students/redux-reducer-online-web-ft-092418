5df6483d2efc05f04d57daa445270bedffe623b1
Peter-G-Stone <peter.stone.online@gmail.com>
2019-02-18 12:52:11 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,55 @@
-export function manageFriends(state, action){
-}
+// my og solution:
+// export function manageFriends(state, action){
+    
+//     let friends = []
+//     switch (action.type) {
+//         case "ADD_FRIEND":
+//             friends = state.friends
+//             friends.push(action.friend)
+//             return {friends: friends}
+//         case "REMOVE_FRIEND":
+//             state.friends.forEach((friend) => {
+//                 if (friend.id !== action.id ){
+//                     friends.push(friend)
+//                 }
+//             })
+//             return {friends: friends}
+            
+//         default:
+//             return state
+//     }
+// }
+
+//solution from learn:
+export function manageFriends(state = {
+    friends: [],
+  }, action) {
+    switch(action.type) {
+  
+      case "ADD_FRIEND":
+        return (
+          {...state,
+              friends: [
+                ...state.friends,
+                action.friend
+              ]
+          }
+      )
+  
+      case "REMOVE_FRIEND":
+        const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+        return (
+          {...state,
+              friends: [
+                ...state.friends.slice(0, removalIndex),
+                ...state.friends.slice(removalIndex + 1)
+              ]
+          }
+        )
+  
+      default:
+        return state;
+  
+    }
+};
+  
