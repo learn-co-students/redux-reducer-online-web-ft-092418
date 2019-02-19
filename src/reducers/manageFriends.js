2eb8c847266006d71e1c1eb393c2866c9380035d
harryFBloch <harryfbloch@gmail.com>
2019-02-19 12:32:02 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
 export function manageFriends(state, action){
+  debugger
+  switch (action.type){
+    case "ADD_FRIEND":
+       return {friends: [...state.friends,action.friend]}
+    case "REMOVE_FRIEND":
+
+        let arr = state.friends.filter(friend => {
+          if(friend.id != action.id){
+            return true
+          }
+        })
+        return {friends: arr}
+    default :
+      return state
+  }
 }
